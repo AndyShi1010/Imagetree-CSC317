b174339de591e81dee0faaad5c5792ee9bbdc11f
Andy Shi <ashi2@mail.sfsu.edu>
2020-11-21 21:00:33 -0800

    Refactored more html and css. Added a header. Created landing page. Fixed some styling.

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,21 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {name:"Andy Shi"});
+  res.render('index', {css: ['landing.css']});
 });
 
 router.get('/home', function(req, res, next) {
   res.render('home');
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+router.get('/login', (req, res, next) => {
+  res.render('login', {css: ['login.css', 'formui.css']});
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('registration');
+  res.render('registration', {css: ['registration.css']});
 });
 
+router.get('/tos', function(req, res, next) {
+  res.render('tos');
+});
 
 
 module.exports = router;
